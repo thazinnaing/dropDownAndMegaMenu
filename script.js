03a32623d0fe07aafca0721d95711fb6910c530c
thazin naing <thazinaingmm@gmail.com>
2023-07-05 17:19:15 +0630

    third commit

--- script.js
+++ script.js
@@ -24,6 +24,26 @@ for(let i=0; i<megaMenu.length; i++){
     creatingLiTag(mega_row,arr[i]);
     
 }
+const leftNav=document.getElementsByClassName("left-nav")[0];
+const rightNav=document.getElementsByClassName("right-nav")[0];
+const middleNav=document.getElementsByClassName("middle-nav");
+const bar=document.getElementsByClassName("bar")[0];
+const xmark=document.getElementsByClassName("xmark")[0];
+bar.addEventListener("click", ()=>{
+    bar.style.display="none";
+    xmark.style.display="block";
+    leftNav.style.display="none";
+    rightNav.style.display="flex";
+
+
+})
+xmark.addEventListener("click", ()=>{
+    xmark.style.display="none";
+    bar.style.display="block";
+    leftNav.style.display="flex";
+    rightNav.style.display="none";
+})
+
 
 
 
